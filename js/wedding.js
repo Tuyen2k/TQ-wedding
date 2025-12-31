710ac0f82ac53cb44422d8c5ca8a7e75902d88ae
tuyen nguyen <tuyen4835@gmail.com>
2026-01-01 00:37:37 +0700

    update reponsive

--- js/wedding.js
+++ js/wedding.js
@@ -1,18 +1,17 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const heart = document.querySelector('.heart');
     const envelopWrapper = document.querySelector('.envelope-wrapper');
-    const chuHy = document.querySelector('.chu-hy');
+    const noteClickHeart = document.querySelector('#note-click-heart');
+    const nameContainer = document.querySelector('#name-co__dau-chu__re');
+    const arrowDown = document.querySelector('#arrow-down__hint');
 
     heart.addEventListener('click', () => {
-        envelopWrapper.classList.toggle('flap');
+        const isOpen = envelopWrapper.classList.toggle('flap');
 
-        if (envelopWrapper.classList.contains('flap')) {
-            envelopWrapper.classList.remove('scale-up-center');
-            envelopWrapper.classList.add('scale-open');
-        } else {
-            envelopWrapper.classList.remove('scale-open');
-            envelopWrapper.classList.add('scale-up-center');
-        }
+        envelopWrapper.classList.toggle('scale-open', isOpen);
+        envelopWrapper.classList.toggle('scale-up-center', !isOpen);
+        noteClickHeart.classList.toggle('d-none', isOpen);
+        nameContainer.classList.toggle('d-none', !isOpen);
+        arrowDown.classList.toggle('d-none', !isOpen);
     });
 });
