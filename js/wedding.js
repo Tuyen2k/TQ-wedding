f9d0b0b45c856adea44e01fbe0e312d51e703f5e
Tuyen2k <tuyen4835@gmail.com>
2025-12-31 15:21:30 +0700

    cấu trúc lại dự án

---
+++ js/wedding.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const heart = document.querySelector('.heart');
+    const envelopWrapper = document.querySelector('.envelope-wrapper');
+
+    heart.addEventListener('click', () => {
+        envelopWrapper.classList.toggle('flap');
+
+        if (envelopWrapper.classList.contains('flap')) {
+            envelopWrapper.classList.remove('scale-up-center');
+            envelopWrapper.classList.add('scale-open');
+        } else {
+            envelopWrapper.classList.remove('scale-open');
+            envelopWrapper.classList.add('scale-up-center');
+        }
+    });
+});
