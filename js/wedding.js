f837dff70145e8635bf55e311460c370483e89be
tuyen nguyen <tuyen4835@gmail.com>
2026-01-09 00:45:32 +0700

    chính thức online thiệp cưới co-dau & chu-re lần 1

--- js/wedding.js
+++ js/wedding.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     {
       root: null,
-      rootMargin: "0px 0px -80px 0px",
+      rootMargin: "0px 0px -20px 0px",
       threshold: 0.1,
     }
   );
